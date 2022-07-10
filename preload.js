595ed282aee4173158c173db875b0fa233c225b8
jasongrass <junjiegrass@qq.com>
2022-07-10 12:22:31 +0800

    utools.plugin.oxford

---
+++ preload.js
@@ -0,0 +1,40 @@
+window.utools.onPluginEnter(({ code, type, payload }) => {
+  console.log("用户进入插件", code, type, payload);
+
+  // 再执行 onPluginEnter
+  // window.alert(`onPluginEnter ${payload}`)
+
+  if (!isValidWord(payload)) {
+    // window.utools.outPlugin();
+    return;
+  }
+
+  const searchUrl = `https://www.oxfordlearnersdictionaries.com/definition/english/${payload}`;
+
+  //   window.utools.ubrowser
+  //     .goto(searchUrl)
+  //     .evaluate(() => {
+  //       window.jQuery("#main-container").off("dblclick");
+  //       window.jQuery("#ox-header").hide();
+  //     })
+  //     .run({ width: 780, height: 600 });
+
+  window.alert(searchUrl);
+
+  // window.utools.shellOpenExternal(searchUrl);
+  window.utools.outPlugin();
+});
+
+window.onload = function () {
+  // 先执行 onload
+  // window.alert(`onload ${word}`)
+};
+
+function isValidWord(payload) {
+  if (!payload) {
+    return false;
+  }
+
+  var r = /^[a-z]+$/gi;
+  return payload.match(r);
+}
